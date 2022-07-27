ee390ae23cda4f6fee469440d544d2089b8b551a
ttt-net <tenten102matsuyama@gmail.com>
2022-07-27 18:00:57 +0900

    Initial Commit

---
+++ H4X0R-News-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4X0R-News-SwiftUI
+//
+//  Created by tetsuta matsuyama on 2022/07/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ H4X0R-News-SwiftUI/H4X0R_News_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_News_SwiftUIApp.swift
+//  H4X0R-News-SwiftUI
+//
+//  Created by tetsuta matsuyama on 2022/07/27.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_News_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
