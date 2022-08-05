6b3c52a8d7679237a0619705a60b15babce8f137
ttt-net <tenten102matsuyama@gmail.com>
2022-08-05 21:06:14 +0900

    Add feature to decode data from JSON

--- H4X0R-News-SwiftUI/Models/NetworkManager.swift
+++ H4X0R-News-SwiftUI/Models/NetworkManager.swift
@@ -9,15 +9,23 @@ import Foundation
 
 class NetworkManager {
     
+    var posts: [Post] = []
+    
     func fetchData() {
         if let url = URL(string: "https://hn.algolia.com/api/v1/search?tags=front_page#") {
             let session = URLSession(configuration: .default)
             let task = session.dataTask(with: url) { (data, response, error) in
-
+                let decoder = JSONDecoder()
                 if error == nil {
                     if let safeData = data {
-                        let dataString = String(data: safeData, encoding: .utf8)
-                        print(dataString)
+                        do {
+                            let results = try decoder.decode(Results.self, from: safeData)
+                            DispatchQueue.main.async {
+                                self.posts = results.hits
+                            }
+                        } catch {
+                            print(error)
+                        }
                     }
                 }
             }
