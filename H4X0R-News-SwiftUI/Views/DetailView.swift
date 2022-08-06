18b42a11dbf3f32d135af5dd1ff795678e34a80c
ttt-net <tenten102matsuyama@gmail.com>
2022-08-06 22:32:13 +0900

    Add feature to present a web view using UIViewRepresentable

--- H4X0R-News-SwiftUI/Views/DetailView.swift
+++ H4X0R-News-SwiftUI/Views/DetailView.swift
@@ -6,16 +6,34 @@
 //
 
 import SwiftUI
+import WebKit
 
 struct DetailView: View {
     let url: String?
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        WebView(urlString: url)
     }
 }
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
         DetailView(url: "https://www.google.com")
+    }
+}
+
+struct WebView: UIViewRepresentable {
+    let urlString: String?
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let safeString = urlString {
+            if let url = URL(string: safeString) {
+                let request = URLRequest(url: url)
+                uiView.load(request)
+            }
+        }
     }
 }
